f720a677cf767c70b81322779a5142202ae33ca9
talksinfinitetech <talksinfinitetech@gmail.com>
2024-05-06 20:02:56 +0530

    Update index.ts

--- index.ts
+++ index.ts
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "*",
+    origin: "https://dns-frontend1.vercel.app/",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
